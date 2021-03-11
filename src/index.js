7c122b49fc1a87e375a7e9d3b9d44ea36278d882
Irina Giruta <irina.giruta@gmail.com>
2021-03-11 23:43:04 +0200

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const open = [], close=[], tempArr=[];
+  let lastEl;
+  let count=0;
+
+  bracketsConfig.forEach(item=>{
+    const brackets = item.join();
+    open.push(brackets[0]);
+    close.push(brackets[2]);
+  })
+
+  for (let i = 0; i < str.length; i++) {
+    if (open.includes(str[i]) && close.includes(str[i]) && count%2 === 0) {
+      tempArr.push(str[i]);
+      count = count + 1;
+    } else if (open.includes(str[i]) && close.includes(str[i]) && count%2 === 1 && tempArr.length) {
+      lastEl = tempArr[tempArr.length - 1];
+      if (lastEl === str[i]) {
+        tempArr.pop();
+        count = count - 1;
+      }
+    } else if (open.includes(str[i])) {
+      tempArr.push(str[i]);
+    } else if (close.includes(str[i])) {
+      if (tempArr.length) {
+        lastEl = tempArr[tempArr.length - 1];
+        open.forEach((item, ind) => {
+          if (lastEl===item && str[i] === close[ind]) {
+            tempArr.pop();
+          }
+        })
+      } else return false;
+    }
+  }
+  return (!tempArr.length) ? true : false;
 }
